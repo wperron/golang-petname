3e7a62a23abec12c1728980621c215f136153e9d
William Perron <hey@wperron.io>
2021-11-20 16:25:38 -0500

    use /dev/urandom to seed rand.Math instead of local time

--- cmd/petname/main.go
+++ cmd/petname/main.go
@@ -22,22 +22,23 @@ package main
 import (
 	"flag"
 	"fmt"
-	"math/rand"
-	"time"
-	"github.com/dustinkirkland/golang-petname"
+	"log"
+
+	petname "github.com/dustinkirkland/golang-petname"
 )
 
 var (
-	words = flag.Int("words", 2, "The number of words in the pet name")
+	words     = flag.Int("words", 2, "The number of words in the pet name")
 	separator = flag.String("separator", "-", "The separator between words in the pet name")
 )
 
 func init() {
-	rand.Seed(time.Now().UTC().UnixNano())
+	if err := petname.NonDeterministicMode(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 	flag.Parse()
-	rand.Seed(time.Now().UnixNano())
 	fmt.Println(petname.Generate(*words, *separator))
 }
